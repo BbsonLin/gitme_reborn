8fdb7d10b9956af58321449d166fc297537a4f99
Bobson Lin <bobson801104@gmail.com>
2019-10-01 16:27:59 +0800

    feat: Day 24 - Toast 訊息

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_progress_hud/flutter_progress_hud.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:github/server.dart';
 import 'package:gitme_reborn/components/github_login_form.dart';
 import 'package:gitme_reborn/services/github_api.dart';
 
@@ -32,15 +34,30 @@ class _LoginPageState extends State<LoginPage> {
                         username: state.usernameController.text,
                         password: state.passwordController.text,
                       );
-                      await githubClient.users.getCurrentUser();
+                      User currentUser =
+                          await githubClient.users.getCurrentUser();
+                      Fluttertoast.showToast(
+                        msg: "Welcome ${currentUser.login} ~~",
+                        toastLength: Toast.LENGTH_LONG,
+                      );
                       Navigator.pushReplacementNamed(context, "/home");
                     } catch (e) {
-                      print(e);
+                      Fluttertoast.showToast(
+                        msg: "Uh oh, your username or password is wrong...",
+                        toastLength: Toast.LENGTH_LONG,
+                        textColor: Colors.black38,
+                        backgroundColor: Colors.deepOrange[300],
+                      );
                     }
                     progress.dismiss();
                   });
                 } else {
-                  print("validate failed...");
+                  Fluttertoast.showToast(
+                    msg: "Please enter required fields.",
+                    toastLength: Toast.LENGTH_LONG,
+                    textColor: Colors.black38,
+                    backgroundColor: Colors.amber,
+                  );
                 }
               },
             ),
